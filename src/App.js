c7c1a7de3c5e0c090912c0f24b348d2f18263c9f
El coco <jesussanjuan23@gmail.com>
2022-11-22 22:24:22 -0500

    todo listo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Principal from './componentes/Principal.jsx';
+function App() {
+  return (
+    <div className="container">
+   <Principal/>
+     </div>
+  );
+}
+
+export default App;
