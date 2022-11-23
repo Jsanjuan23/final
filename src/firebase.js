f80f557228257c1dfb9d058ea1298e8454814db5
El coco <jesussanjuan23@gmail.com>
2022-11-22 22:52:39 -0500

    modificando

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB7BBnZqsYhyzkxH0YOod-j7w3nVNz8sLg",
-    authDomain: "medical-231cb.firebaseapp.com",
-    projectId: "medical-231cb",
-    storageBucket: "medical-231cb.appspot.com",
-    messagingSenderId: "409573091146",
-    appId: "1:409573091146:web:b3c671aa217229eefe83b3"
+    apiKey: "AIzaSyBV6P02ziTzT7HMhJfYH7P86-lqOm-rj1M",
+  authDomain: "medical-analysis-d66a0.firebaseapp.com",
+  projectId: "medical-analysis-d66a0",
+  storageBucket: "medical-analysis-d66a0.appspot.com",
+  messagingSenderId: "55226716247",
+  appId: "1:55226716247:web:5013513f693b842e121312"
 };
 
 // Initialize Firebase
