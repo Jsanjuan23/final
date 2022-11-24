92986b2015121cd3b81fcee5e39a83602b14e8a2
El coco <jesussanjuan23@gmail.com>
2022-11-23 20:13:15 -0500

    mdf_bd

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBV6P02ziTzT7HMhJfYH7P86-lqOm-rj1M",
-  authDomain: "medical-analysis-d66a0.firebaseapp.com",
-  projectId: "medical-analysis-d66a0",
-  storageBucket: "medical-analysis-d66a0.appspot.com",
-  messagingSenderId: "55226716247",
-  appId: "1:55226716247:web:5013513f693b842e121312"
+  apiKey: "AIzaSyCELaEe6U5zQCb41R-1rL1xO8wQuSdlTB4",
+  authDomain: "analisis-a29ee.firebaseapp.com",
+  projectId: "analisis-a29ee",
+  storageBucket: "analisis-a29ee.appspot.com",
+  messagingSenderId: "665397395756",
+  appId: "1:665397395756:web:eb0587510b16430326ac89"
 };
 
 // Initialize Firebase
